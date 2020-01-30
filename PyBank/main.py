54c88945607c89786a2251dec19096256e29dd28
tanishablakely <57075805+tanishablakely@users.noreply.github.com>
2020-01-30 14:17:57 -0600

    Final Code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,32 +1,55 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#%%writefile main.py
+
+# Store the file path associated with the file (note the backslash may be OS specific)
+import os
+import csv
+
+csv_path = '03-Python_budget_data.csv'
+
+with open(csv_path) as csv_file:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    # Skip Header
+    csv_header = next(csv_reader)
+    total_months = 0
+    total_amount = 0
+    previous_amount = 0
+    current_amount = 0
+    changes = []
+    total_change = 0
+    average_change = 0 
+    max_change = 0
+    min_change = 0
+    
+    print ("Financial Analysis\n----------------")
+
+    for row in csv_reader:
+        current_amount = row[1]
+        
+        #Calculate change in "Profit/Losses" from a previous period after the first month.
+        if total_months > 0:
+            changes.append(int(current_amount) - int(previous_amount))
+        # The total number of months included in the dataset
+        total_months = total_months + 1
+        
+        # The net total amount of "Profit/Losses" over the entire period
+        total_amount = total_amount + int(current_amount)
+        
+        # Set value for the next iteration
+        previous_amount = current_amount
+    #for (month, change) in changes.items():
+    for change in changes:
+        # The total change in "Profit/Losses"
+        total_change = total_change + change
+        
+    #The average of the changes in "Profit/Losses" over the entire period
+    average_change = total_change/total_months
+        
+    #Print results
+    print (f"Total Months: {total_months}")
+    print (f"Total: ${total_amount}")
+    print (f"Average Change: ${round(average_change,2)}")
+    print (f"Greatest Increase in Profits: ${max(changes)}")
+    print (f"Greatest Decrease in Profits: ${min(changes)}")
--- PyPoll/.ipynb_checkpoints/main-checkpoint.py
+++ PyPoll/.ipynb_checkpoints/main-checkpoint.py
@@ -1,32 +1,51 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#%%writefile main.py
+import os
+import csv
+with open('election_data.csv', 'r') as csv_file:
+    csv_reader_object = csv.reader(csv_file, delimiter=',')
+
+#Set Variables
+    #Skip Header
+    header = next(csv_reader_object)
+    
+    # The total number of votes cast
+    total_votes = 0
+    
+    # Track Candidate Votes
+    candidates = dict()
+    winner = ''
+    
+    for row in csv_reader_object:
+        total_votes = total_votes + 1
+        if row[2] in candidates:
+            candidates[row[2]] = candidates[row[2]] + 1
+        else:
+            candidates[row[2]] = 1
+    # The winner of the election based on popular vote.        
+    winner = max(candidates.items(), key=lambda x : x[1])
+
+    # Print Election Results
+
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------")
+    
+   
+    # The percentage and total number of votes each candidate won
+    for candidate, votes in candidates.items():
+        print(f"{candidate}: {round(votes*100/total_votes,3)}% ({votes})")
+    print("-------------------------")
+    print(f"Winner: {winner[0]}")
+    print("-------------------------")
+    
+    # Final script should both print the analysis to the terminal and export a text file with the results.
+    with open('results.txt', 'w') as output_file: 
+        output_file.write("Election Results\n-------------------------\n")
+        output_file.write(f"Total Votes: {total_votes}\n")
+        output_file.write("-------------------------\n")
+        for candidate, votes in candidates.items():
+            output_file.write(f"{candidate}: {round(votes*100/total_votes,3)}% ({votes})\n")
+        output_file.write("-------------------------\n")    
+        output_file.write(f"Winner: {winner[0]}\n")
+        output_file.write("-------------------------")
